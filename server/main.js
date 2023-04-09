5a4b42c38445f6bdb5b5973cb4953d9831487519
Abhishek <aabhishekbbhardwaj@gmail.com>
2023-04-09 14:50:58 +0530

    Structured project

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 const Server = require('./server');
 const ngrok = require('ngrok');
+// const { NGROK_AUTH } = require('./env/env-dev');
 const PORT = 5151;
-const NGROK_AUTH = '25EqWHVn2o4EaXW3udGsyagA8wg_4LwKEPfSdFvT8vy2jUN92'; // abhardwaj1@kloudrac.com (https://ngrok.com)
 
 Server.listen(PORT, async () => {
     // const url = await ngrok.connect({  addr: PORT, authtoken: NGROK_AUTH });
